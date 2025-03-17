51154f5949a41c86f283ee5c85aea87a45ad9137
adrianofpereira <nanojau@gmail.com>
2025-03-17 10:55:25 -0300

    Pré-Processamento dos Dados / Divisão da base de Perguntas e Respostas

---
+++ chatbot.py
@@ -0,0 +1,34 @@
+# Construção do Chatbot com Deep NLP
+
+# importação das bibliotecas
+import numpy as np
+import tensorflow as tf
+import re
+import time
+
+linhas = open('movie_lines.txt', encoding='utf-8',
+              errors='ignore').read().split('\n')
+conversas = open('movie_conversations.txt', encoding='utf-8',
+                 errors='ignore').read().split('\n')
+
+# Criação de um dicionário para mapear cada linha do seu ID
+
+
+id_para_linha = {}
+
+
+for linha in linhas:
+    #print(linha)
+    _linha = linha.split(' +++$+++ ')
+    #print(_linha)
+    if len(_linha) == 5:
+        print(_linha[4])
+        id_para_linha[_linha[0]] = _linha[4]
+        
+#Criação de uma lsita com todas as conversas
+conversas_id = []
+for conversa in conversas[:-1]:
+    #print(conversa)
+    _conversa = conversa.split(' +++$+++ ')[-1][1:-1].replace("'","").replace(" ","")
+    #print(_conversa)
+    conversas_id.append(conversa.split(','))
